e2c7f783a1b78d1db1e3a4fa60102ca3a484ea56
Zakaria Adriouche13 <sirod898@gmail.com>
2024-08-14 13:06:31 +0100

    done with ft_atod

--- testing/oho.c
+++ testing/oho.c
@@ -7,16 +7,6 @@
 
 # define  INT_MIN_UD 2147483648
 
-// int ft_strlen(const char *str)
-// {
-//     int i;
-
-//     i = 0;
-//     while (str[i])
-//         i++;
-//     return (i);
-// }
-
 static int	ft_isspace(const char c)
 {
 	return (c == ' ' || c == '\t' || c == '\v' || c == '\r' || c == '\f'
@@ -27,122 +17,45 @@ static int is_nb(const char c)
 {
 	return (c >= '0' && c <= '9');
 }
-static int	skip(const char *str, size_t *index)
+
+double	ft_atoi(const char *str, int *sign, int *dot)
 {
-	int		sgn;
-	size_t	i;
-
-	i = 0;
-	sgn = 1;
-	while (ft_isspace(str[i]))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sgn = -1;
-		i++;
-	}
-	*index = i;
-	return (sgn);
-}
-
-double	ft_atoi(const char *str, int *sign, int *error)
-{
-	double	res;
-	size_t	i;
-
+	double 	res;
+	int		i;
+	// "-/+/eps|xxx|.|yyy"
 	res = 0;
-	*sign = skip(str, &i);
-    // if (str[i] == '.' && (str[i+1] >= '0' && str[i+1] <= '9'))
-	// {
-    //     *error = 0;
-	// }
-	while (str[i] >= '0' && str[i] <= '9')
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i = 1;
+	if (str[0] == '-')
+		*sign = -1;
+	while (is_nb(str[i]))
 	{
 		res = (res * 10) + (str[i] - '0');
-        *error = 0;
 		i++;
 	}
-    // if (str[i] != '.' && str[i] != 32)
-    //     return (0.0);
-	return  (res);  //(res * sign);
+	if (str[i] == '.')
+		*dot = 1;
+	return (res);
 }
 
-static void	skip_rev(const char *str, size_t *index)
+double	ft_after_dot(const char *str, int dot)
 {
-	size_t	i;
-
+	double 	res;
+	int		i;
+	// "-/+/eps|xxx|.|yyy"
+	res = 0;
+	if (dot == 0)
+		return (0.0);
 	i = ft_strlen(str) - 1;
-	while (ft_isspace(str[i]))
-		i--;
-	*index = i;
-}
-
-// double after_dot(const char *str, int b_exist)
-// {
-//     double	res;
-//     int     a_exist;
-// 	size_t	i;
-
-// 	res = 0.0;
-//     a_exist = 0;
-// 	skip_rev(str, &i);
-// 	while (str[i] >= '0' && str[i] <= '9')
-// 	{
-//         a_exist = 1;
-// 		res = (res / 10) + (str[i] - '0');
-// 		i--;
-// 	}
-//     //if (str[i] == '.' && ((str[i-1] == 32 && b_exist == 1) || (str[i-1] >= '0' && str[i-1] <= '9')))
-//     if (str[i] == '.' && (b_exist == 0 && ((str[i-1] >= '0' && str[i-1] <= '9')) || (b_exist == 1 && str[i-1] == 32)))
-// 	    res /= 10;
-//     else
-//         res = 0.0;
-//     return (res);
-// }
-
-double after_dot(const char *str, int b_exist)
-{
-    double	res;
-    // int     a_exist;
-	size_t	i;
-
-	res = 0.0;
-    // a_exist = 0;
-	skip_rev(str, &i);
-	while (str[i] >= '0' && str[i] <= '9')
+	while (is_nb(str[i]))
 	{
-        // a_exist = 1;
 		res = (res / 10) + (str[i] - '0');
 		i--;
 	}
-    //if (str[i] == '.' && ((str[i-1] == 32 && b_exist == 1) || (str[i-1] >= '0' && str[i-1] <= '9')))
-    if (str[i] == '.') //   && (b_exist == 0 && ((str[i-1] >= '0' && str[i-1] <= '9')) || (b_exist == 1 && str[i-1] == 32))
-	    res /= 10;
-    else
-        res = 0.0;
-    return (res);
+	res /= 10;
+	return (res);
 }
-
-// int valid_num(char *str)
-// {
-//     int i;
-
-//     i = 0;
-//     while (ft_isspace(str[i]))
-//         i++;
-//     while ((str[i-1] >= '0' && str[i-1] <= '9'))
-//     {
-//         i++;
-//     }
-//     if (str[i] == '.')
-//         i++;
-//     while ((str[i-1] >= '0' && str[i-1] <= '9'))
-//     {
-//         i++;
-//     }
-//     return (42);
-// }
 
 int one_single_dot(const char *str)
 {
@@ -165,9 +78,6 @@ int one_single_dot(const char *str)
 			m++;
 		i++;
 	}
-	// printf("dot == %d\n", dot);
-	// printf("+ == %d\n", p);
-	// printf("- == %d\n", m);
 	if ((dot < 2) && ((p == 1 && m == 0) || (p == 0 && m == 1) || (p == 0 && m == 0)))
 		return (1);
 	else
@@ -258,56 +168,35 @@ int valid_order(char *str)
 //  .-  : X
 // +.  : X
 
-double  ft_atod(const char *str)
-{
-    int     error;
-    int     sign;
-    double  res;
-	char 	*s;
 
-	if (!str_is_valide(str))
-		return (-1);
-	s = ft_strtrim(str, " ");
-	if (!valid_order(s))
-		return (-2);
-	// some logic + review atoi and after dot
-    error = 1;
-    res = ft_atoi(str, &sign, &error);
-	printf("atoi = %.2f \n", res);
-    // if (error)
-    //     return (0.0);
-	printf("after_dot = %.2f \n", after_dot(str, error));
-	int i = 0;
-	int exist_a = 0;
-	while (str[i])
+double	ft_atod(char *str)
+{
+	double 	res;
+	char 	*s;
+	int 	dot;
+	int 	mp;
+
+	s = NULL;
+	mp = 1;
+	dot = 0;
+	res = 0.0;
+	if (str_is_valide(str))
 	{
-		if (str[i] == '.')
+		s = ft_strtrim(str, " ");
+		if (valid_order(s))
 		{
-			printf("str[%d] = %c \n", i +1, str[i+1]);
-			if (is_nb(str[i+1]))
-				exist_a = 1;
-			break;
+			res = ft_atoi(s, &mp, &dot);
+			printf(" before . res = %.2f\n", res);
+			res += ft_after_dot(s, dot);
+			printf(" after . res = %.2f\n", res);
 		}
-		i++;
+		free(s);
 	}
-	if (exist_a = 1)
-    	res += after_dot(str, error);
-    res = res * sign;
-	free(s);
-    return (res);
+	return (res * mp);
 }
 
 int main(void)
 {
-
-    // double b;
-	// if (one_single_dot(str) == 1)
-	// {
-	// 	printf("one signle dot here: %s\n", str);
-	// 	printf("func : %d\n", one_single_dot(str));
-	// }
-	// else
-	// 	printf("there imposters here : %s\n", str);
 
 //  13
 //  13. 
@@ -323,40 +212,9 @@ int main(void)
 
 	double a;
 
-	a = ft_atod("  -3.25  ");
+	a = ft_atod(" - 3    ");
 	printf("\n\na = %.2f \n", a);
-	// char *str = "  .+  ";
 
-	// if (str_is_valide(str) == 1)
-	// 	printf("\"%s\" : str is VALID (1)\n", str);
-	// else
-	// 	printf("\"%s\" : str is NOT valid (STOP)\n", str);
-	// char *soso = ft_strtrim(str, " ");
-	// if (valid_order(soso) == 1 && str_is_valide(str))
-	// {
-	// 	printf("\"%s\"     : order is VALID (2)\n", soso);
-	// 	printf("YOU CAN PASS TO ITOD now!\n");
-	// }
-	// else if (str_is_valide(str))
-	// 	printf("\"%s\"     : order is NOT valid (STOP)\n", soso);
-
-	// a = ft_atod("   .1337.4242   ");
-	// if (a == -1)
-	// 	printf("was not valid\n");
-	// else
-	// 	printf("success a == %f\n", a);
-
-	// is_str_valide("   +1337.4242   ");
-	// if (valid_order("-.37"))
-	// 	printf("order -> VALID\n");
-	// else
-	// 	printf("order -> NOT Valid\n");
-
-    // valid_double("  13 .42  ");
-    //printf ("a = %f \n", a);
-    // b = ft_atod("   ^     42.1337           ");
-    // b += a;
-    // printf("b = %f \n", b);
 
     return (0);
 }
